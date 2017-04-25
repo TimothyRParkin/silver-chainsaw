0e445ceccf05bcd57006f13bb9a9058138fac793
TimothyRParkin <t.parkin6869@student.leedsbeckett.ac.uk>
2017-04-25 15:29:07 +0100

    Completed Web stuff

--- src/main/java/hello/OpinionService.java
+++ src/main/java/hello/OpinionService.java
@@ -1,13 +1,18 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class OpinionService {
-
+    private static final Logger log = LoggerFactory.getLogger(OpinionService.class);
     @Autowired
     protected OpinionsEntryRepository opinionsEntryRepository;
 
@@ -26,4 +31,13 @@ public class OpinionService {
     public OpinionsEntry findOne(Long id) {
         return opinionsEntryRepository.findOne(id);
     }
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
+    }
+
 }
+
